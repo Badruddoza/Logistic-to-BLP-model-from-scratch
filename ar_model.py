dd236170cc0f3dc413f8391f9535bdcefe1c7c5d
Syed Badruddoza <s.badruddoza@yahoo.com>
2019-06-14 22:51:22 -0700

    Update ar_model.py

--- ar_model.py
+++ ar_model.py
@@ -3,6 +3,6 @@ import statsmodels.tsa.api as smtsa
 n=600
 # Generate AR(1) dataset
 ar = np.r_[1, -0.6]
-mal = np.r_[1, 0]
+ma = np.r_[1, 0]
 ar1_data = smtsa.arma_generate_sample(ar=ar, ma=ma, nsample=n)
 plotds(ar1_data)
