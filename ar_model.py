2afc04f165c0bb5936927efb18ba9b1d894ce87f
Syed Badruddoza <s.badruddoza@yahoo.com>
2019-06-16 08:25:18 -0700

    Update ar_model.py

--- ar_model.py
+++ ar_model.py
@@ -1,8 +1,10 @@
 import statsmodels.tsa.api as smtsa
+import numpy as np
 # Number of samples
 n=600
 # Generate AR(1) dataset
 ar = np.r_[1, -0.6]
 ma = np.r_[1, 0]
 ar1_data = smtsa.arma_generate_sample(ar=ar, ma=ma, nsample=n)
+# Plot the AR1 process
 plotds(ar1_data)
